568d1e73f83cf3e5048ee9004935f4cc85d670e6
mena-hub <mssz.nnia@gmail.com>
2021-01-22 16:14:40 -0300

    Revoke those tokens if a user logs out

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,4 @@
-from flask import redirect
+from flask import redirect, make_response
 from flask_jwt_extended import (jwt_required, jwt_refresh_token_required, jwt_optional, fresh_jwt_required, get_raw_jwt, get_jwt_identity, 
                                 create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies, unset_jwt_cookies, unset_access_cookies)
 from application.app import create_app
@@ -12,6 +12,11 @@ def assign_access_refresh_tokens(user_id, url):
     response = make_response(redirect(url, 302))
     set_access_cookies(response, access_token)
     set_refresh_cookies(response, refresh_token)
+    return response
+
+def unset_jwt():
+    response = make_response(redirect(app.config['BASE_URL'] + '/', 302))
+    unset_jwt_cookies(response)
     return response
 
 if __name__ == '__main__':
