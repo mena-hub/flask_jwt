279ea0228caa985fe3940e66c52f8eba6eb5cd5f
mena-hub <mssz.nnia@gmail.com>
2021-01-22 16:53:46 -0300

    Pass tokens into RESTful API resources

--- backend/application/app.py
+++ backend/application/app.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from flask_restful import Api
 from .config import Config
 from .extensions import jwt
+from resources.token import AccountSettings
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     register_extensions(app)
+    register_resources(app)
     return app
 
 def register_extensions(app):
     jwt.init_app(app)
+
+def register_resources(app):
+    api = Api(app)
+    api.add_resource(AccountSettings, "/account/change_settings")
---
+++ backend/resources/token.py
@@ -0,0 +1,9 @@
+from flask import render_template
+from flask_restful import Resource
+from flask_jwt_extended import fresh_jwt_required, get_jwt_identity
+
+class AccountSettings(Resource):
+    @fresh_jwt_required
+    def post(self):
+        username = get_jwt_identity()
+        # Important setting change commits 
