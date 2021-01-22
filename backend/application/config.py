7588f81a4e38999efb400344f73213caed44bb6a
mena-hub <mssz.nnia@gmail.com>
2021-01-22 17:04:02 -0300

    Fix JWT Error in API resources

--- backend/application/config.py
+++ backend/application/config.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 class Config:
     DEBUG = True
     BASE_URL = "http://127.0.0.1:5000"
+    PROPAGATE_EXCEPTIONS = True
     JWT_SECRET_KEY = 'random-string'
     JWT_TOKEN_LOCATION = ['cookies']
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds = 120)
