7a4e512db7499cf81207b20bc01fc4f78e55188b
mena-hub <mssz.nnia@gmail.com>
2021-01-22 15:41:38 -0300

    Set up JWTManager

--- backend/application/app.py
+++ backend/application/app.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from .extensions import jwt
 
 def create_app():
     app = Flask(__name__)
+    register_extensions(app)
     return app
+
+def register_extensions(app):
+    jwt.init_app(app)
