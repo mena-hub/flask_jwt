4e41033aa9ea9e782ade978af00a0da40e67325d
mena-hub <mssz.nnia@gmail.com>
2021-01-22 16:08:20 -0300

    Separate your JWT handlers on a different file

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,7 @@
+from flask_jwt_extended import (jwt_required, jwt_refresh_token_required, jwt_optional, fresh_jwt_required, get_raw_jwt, get_jwt_identity, 
+                                create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies, unset_jwt_cookies, unset_access_cookies)
 from application.app import create_app
+from application.extensions import jwt
 
 app = create_app()
 
