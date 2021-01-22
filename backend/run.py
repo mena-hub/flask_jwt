9f6b2705ffac369ebcc974d0faf883ee2935625d
mena-hub <mssz.nnia@gmail.com>
2021-01-22 16:22:10 -0300

    Use @jwt decorator to handle user operations

--- backend/run.py
+++ backend/run.py
@@ -6,6 +6,25 @@ from application.extensions import jwt
 
 app = create_app()
 
+@jwt.unauthorized_loader
+def unauthorized_callback(callback):
+    # No auth header
+    return redirect(app.config['BASE_URL'] + '/', 302)
+
+@jwt.invalid_token_loader
+def invalid_token_callback(callback):
+    # Invalid Fresh/Non-Fresh Access token in auth header
+    response = make_response(redirect(app.config['BASE_URL'] + '/'))
+    unset_jwt_cookies(response)
+    return response, 302
+
+@jwt.expired_token_loader
+def expired_token_callback(callback):
+    # Expired auth header
+    response = make_response(redirect(app.config['BASE_URL'] + '/token/refresh'))
+    unset_access_cookies(response)
+    return response, 302
+
 def assign_access_refresh_tokens(user_id, url):
     access_token = create_access_token(identity=str(user_id), fresh=True)
     refresh_token = create_refresh_token(identity=str(user_id))
